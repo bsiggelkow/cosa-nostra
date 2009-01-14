6154ecbcb598911c0e895801674628418f69f520
Desi McAdam a.k.a The Chick <desi.mcadam@gmail.com>
2009-01-14 09:14:34 -0500

    Desi - trying to add view specs

--- spec/views/families/show.html.haml_spec.rb
+++ spec/views/families/show.html.haml_spec.rb
@@ -1,4 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "/families/show.html.haml" do
+describe "/families/1/show.html.haml" do
+  before(:each) do
+    @family = Factory(:family)
+    assigns[:families] = [@family]
+  end
+  
+  it "should render" do
+    do_render
+  end
+  
+  it "should have a family name" do
+    template.should have_text(@family.name)
+  end
+
+  def do_render
+    render_template "/families/#{@family.id}"
+  end
 end
