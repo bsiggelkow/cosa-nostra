ef916e14bf013900aa4523ef8599688d8573dfc2
Desi McAdam a.k.a The Chick <desi.mcadam@gmail.com>
2009-01-14 15:00:08 -0500

    commiting change to show

--- spec/views/families/show.html.haml_spec.rb
+++ spec/views/families/show.html.haml_spec.rb
@@ -3,14 +3,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "/families/1/show.html.haml" do
   before(:each) do
     @family = Factory(:family)
-    assigns[:families] = [@family]
+    @user = Factory :user, :family => @family
+    template.stubs(:current_user).returns(@user)
+    assigns[:family] = @family
   end
   
   it "should render" do
     do_render
   end
+  
+  it "should have text of the family name" do
+    do_render
+    response.should have_text(@family.name)
+  end
+  
+  it "should have an h3 with the family name" do
+    do_render
+    response.should have_tag("<h3>")
+  end
 
   def do_render
-    render_template "/families/#{@family.id}"
+    render :template =>"/families/show"
   end
 end
