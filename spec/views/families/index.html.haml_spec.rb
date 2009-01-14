90186f7b40acee190906fdb61ef53f83e1d796c1
Desi McAdam a.k.a The Chick <desi.mcadam@gmail.com>
2009-01-14 18:29:47 -0500

    Desi - adding additional view specs and some pending specs

--- spec/views/families/index.html.haml_spec.rb
+++ spec/views/families/index.html.haml_spec.rb
@@ -2,12 +2,59 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/families/index.html.haml" do
   before(:each) do
-    @family = Factory(:family)
-    assigns[:families] = [@family]
+    @jordan = Factory(:family, :name => "Jordan")
+    @mcadam = Factory(:family, :name => "McAdam")
+    families = [@jordan, @mcadam]
+    
+    families.each do |family|
+      # create 3 dead ones 
+      3.times do 
+        @user = Factory(:user, :family => family)
+        @user.kill!
+      end
+      # create 2 living
+      2.times do 
+        @user = Factory(:user, :family => family)
+      end
+    end
+
+    assigns[:families] = families
   end
   
   it "should render" do
     do_render
+  end
+  
+  it "should have a heading" do
+    do_render
+    response.should have_tag('h3', "Family List")
+  end
+  
+  it "should have the families names" do
+    do_render
+    response.should have_tag('tr') {
+      with_tag('td', "Jordan")
+      with_tag('td', "McAdam")
+    }
+  end
+  
+  it "should have a link to the family name" do
+    do_render
+    response.should have_tag('a[href=?]', family_path(@jordan))
+  end
+  
+  it "should have a count of the alive members for one family" do
+    do_render
+    response.should have_tag('tr') {
+      with_tag('td', "2")
+    }
+  end
+  
+  it "should have a count of the deceased members for one family" do
+    do_render
+    response.should have_tag('tr') {
+      with_tag('td', "3")
+    }
   end
 
   def do_render
--- spec/views/families/show.html.haml_spec.rb
+++ spec/views/families/show.html.haml_spec.rb
@@ -12,14 +12,49 @@ describe "/families/1/show.html.haml" do
     do_render
   end
   
-  it "should have text of the family name" do
-    do_render
-    response.should have_text(@family.name)
-  end
-  
   it "should have an h3 with the family name" do
     do_render
-    response.should have_tag("<h3>")
+    response.should have_tag("h3", @family.name)
+  end
+  
+  it "should have status" do
+    do_render
+    response.should have_tag("tr") {
+      with_tag("td", @family.users.first.state)
+    }
+  end
+  describe "when user logged in is viewing information about the other family" do
+    describe "and when a family member of the other family is alive" do
+      describe "and a hit has not been issued" do
+        it "should have an 'Issue Hit' link when logged in user is boss"
+        it "should not have an 'Issue Hit' link when logged in as a mobster"
+        it "should have actions blank when logged in as a mobster"
+      end
+    
+      describe "and a hit has been issued" do
+        it "should have an 'Accept Hit' link when logged in user is mobster"
+        it "should not have an 'Accept Hit' link when logged in user is a boss"
+        it "should have actions as 'unassigned' when logged in as boss"
+      end
+    
+      describe "and a hit has been accepted" do
+        it "should have 'Fail' link when logged in user is mobster"
+        it "should have 'Complete' link when logged in user is mobster"
+        it "should have actions as 'assigned' for all users"
+      end
+    
+      describe "and a hit has failed" do
+        it "should have an 'Accept Hit' link when logged in as mobster"
+        it "should not have an 'Accept Hit' link when logged in as a boss"
+        it "should have actions as 'failed'"
+      end
+    end
+    
+    describe "and when a family member of the other family is deceased" do
+      it "should have deceased for the status for all users"
+      it "should have actions set to 'completed' for all users"
+      it "should have who they were killed by for all users"
+    end
   end
 
   def do_render
