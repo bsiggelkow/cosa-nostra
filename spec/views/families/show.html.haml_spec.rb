e3f6863c561952bf911a649a0cc292887ee06065
Bill Siggelkow <bsiggelkow@r2d2.corp.hpcinteractive.com>
2009-01-16 13:15:32 -0500

    made family show view spec pass

--- spec/views/families/show.html.haml_spec.rb
+++ spec/views/families/show.html.haml_spec.rb
@@ -1,6 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
+
 describe "/families/1/show.html.haml" do
+  include UsersHelper
+  
   before(:each) do
     @family = Factory(:family)
     @user = Factory :user, :family => @family
@@ -12,8 +15,20 @@ describe "/families/1/show.html.haml" do
     do_render
   end
   
-  it "should have an h3 with the family name"  
-  it "should have status"
+  it "should have an h3 with the family name"  do
+    do_render
+    response.should have_tag("h3",:count => 1)
+    response.should have_tag("h3",:text => @family.name, :count => 1)
+  end
+  
+  it "should have status" do
+    do_render
+    response.should have_tag("table") do
+      with_tag("tr:nth-child(2)") do
+        with_tag("td:nth-last-child(2)", status(@family.users.first))
+      end
+    end
+  end
 
   def do_render
     render :template =>"/families/show"
