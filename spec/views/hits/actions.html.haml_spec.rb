7ba42c7aa71f18e20f86e877d1f355de5eb5b985
Desi McAdam a.k.a The Chick <desi.mcadam@gmail.com>
2009-01-15 22:22:33 -0500

    Desi - adding view spec yuck

--- spec/views/hits/actions.html.haml_spec.rb
+++ spec/views/hits/actions.html.haml_spec.rb
@@ -6,16 +6,24 @@ describe "hits/_actions.html.haml" do
   end
   
   it "should render" do
-    template.expects(:can_issue_hit).returns(true)
+    template.expects(:can_accept_hit?).at_least(1).returns(false)
+    template.expects(:can_complete_hit?).at_least(1).returns(false)
+    template.expects(:can_fail_hit?).at_least(1).returns(false)
+    template.expects(:can_issue_hit?).returns(true)
     do_render
   end
   
   describe "when user logged in is viewing information about the other family" do
     describe "and when a family member of the other family is alive" do
       describe "and a hit has not been issued" do
-        it "should have an 'Issue Hit' link when logged in user is boss" do
-          template.expects(:can_issue_hit).at_least(1).returns(true)
-          stubs(:current_user).returns(Factory :user)
+        before(:each) do
+          template.expects(:can_accept_hit?).returns(false)
+          template.expects(:can_complete_hit?).returns(false)
+          template.expects(:can_fail_hit?).returns(false)
+        end
+        
+        it "should have an 'Issue Hit' link when permission to issue hit is true" do
+          template.expects(:can_issue_hit?).at_least(1).returns(true)
           do_render
           response.should have_tag('a[href=?]', new_hit_path(:target_id => @desi.id))
 
@@ -23,41 +31,44 @@ describe "hits/_actions.html.haml" do
           # response.should have_tag('a[href$=?]', new_hit_path(:target_id => @desi.id))
         end
 
-        it "should not have an 'Issue Hit' link when logged in as a mobster" do
-          template.expects(:can_issue_hit).at_least(1).returns(false)
-          stubs(:current_user).returns(@mobster)
+        it "should not have an 'Issue Hit' link when permission to issue hit is false" do
+          template.expects(:can_issue_hit?).at_least(1).returns(false)
           do_render
           response.should_not have_tag('a[href=?]', new_hit_path(:target_id => @desi.id))
         end
-
-        it "should have actions blank when logged in as a mobster" do
-          
+      end
+    
+      describe "and a hit is ready to be accepted" do
+        before do 
+          template.expects(:can_issue_hit?).returns(false)
+          template.expects(:can_complete_hit?).returns(false)
+          template.expects(:can_fail_hit?).returns(false)
         end
+        
+        it "should have an 'Accept Hit' link when can accept hit is true" do
+          template.expects(:can_accept_hit?).returns(true) 
+          user = Factory :user
+          
+          hit = Factory :hit, :target => @desi, :assigned_to => user
+          @desi.expects(:target_hit).returns(hit)
+          do_render
+          response.should have_tag('a[href=?]', accept_hit_path(hit))
+        end
+        
+        it "should not have an 'Accept Hit' link when can accept hit is false"
       end
     
-      describe "and a hit has been issued" do
-        it "should have an 'Accept Hit' link when logged in user is mobster"
-        it "should not have an 'Accept Hit' link when logged in user is a boss"
-        it "should have actions as 'unassigned' when logged in as boss"
+      describe "and a hit is ready to be completed" do
+        # These are left for you to complete if you want
+        it "should have 'Complete' link when can complete is true"
+        it "should not have 'Complete' link when can complete is false"
       end
-    
-      describe "and a hit has been accepted" do
-        it "should have 'Fail' link when logged in user is mobster"
-        it "should have 'Complete' link when logged in user is mobster"
-        it "should have actions as 'assigned' for all users"
+      
+      describe "and a hit is ready to be failed" do
+        # These are left for you to complete if you want
+        it "should have 'Fail' link when can fail is true"
+        it "should not have 'Fail' link when can fail is true"
       end
-    
-      describe "and a hit has failed" do
-        it "should have an 'Accept Hit' link when logged in as mobster"
-        it "should not have an 'Accept Hit' link when logged in as a boss"
-        it "should have actions as 'failed'"
-      end
-    end
-    
-    describe "and when a family member of the other family is deceased" do
-      it "should have deceased for the status for all users"
-      it "should have actions set to 'completed' for all users"
-      it "should have who they were killed by for all users"
     end
   end
   
